1e9a955e73310c72736fecc311c0e149c25fab65
Shiva Sathwik <shivasathwik74@gmail.com>
2025-11-29 23:58:32 +0530

    initial

---
+++ apps/trading_engine/src/main.rs
@@ -0,0 +1,8 @@
+﻿// Day 1: Foundation
+const EXPECTED_ENV: &str = "TESTNET";
+
+#[tokio::main]
+async fn main() {
+    println!("Initializing HFT Engine...");
+    println!("Environment Guard: OK ({})", EXPECTED_ENV);
+}
